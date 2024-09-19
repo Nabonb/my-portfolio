5a4c79c27fd84e9449766b3ed36ec5ae17f281ec
Nabonb <nabobaruanb@gmail.com>
2024-09-19 13:44:03 +0600

    Hero sectio pc setup done

---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,51 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../Components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../Components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+
+const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Adrian <span className="waving-hand">🙋‍♂️</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+      <div className="w-full sm:mt-12 h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 20]}
+            ></PerspectiveCamera>
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+            ></HackerRoom>
+            <ambientLight intensity={1}></ambientLight>
+            <directionalLight
+              position={[10, 10, 10]}
+              intensity={0.5}
+            ></directionalLight>
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
