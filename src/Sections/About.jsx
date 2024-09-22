a57a31ef55cb4560b6aa3e9fb4196a10a960f535
Nabonb <nabobaruanb@gmail.com>
2024-09-22 14:56:38 +0600

    about section implemented

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,5 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
+import Globe from "react-globe.gl";
+import Button from "../Components/Button.jsx";
+import { useState } from "react";
+
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleEmailCopy = () => {
+    navigator.clipboard.writeText("nabobaruanb@gmail.com");
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 1500);
+  };
+
   return (
     <section className="c-space my-20">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
@@ -34,6 +49,84 @@ const About = () => {
                 Next.js ecosystem. With variety of frameworks, and tools that
                 allow me to build robust and scalable applications.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-full flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageUrl={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 23.685,
+                    lng: 90.3563,
+                    text: "Hey, It's Me Pratham, Im Here In Bangladesh!!!",
+                    color: "white",
+                    size: 120,
+                  },
+                ]}
+              ></Globe>
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I can work remotely across most timezones
+              </p>
+              <p className="grid-subtext">
+                I'm based in Bangladesh, with the remote work available.
+              </p>
+              <Button
+                name={"Contact Me"}
+                isBeam
+                containerClass="w-full mt-10"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code.
+                Programming isn&apos;t just my profession—it&apos;s my passion.
+                I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact Me</p>
+              <div className="copy-container" onClick={handleEmailCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  nabobaruanb@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
